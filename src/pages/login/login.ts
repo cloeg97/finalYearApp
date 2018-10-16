af8e4e0bb8f0f1135e682f11057d4b6554da1a08
Cloe <cloe@github.com>
2018-10-16 18:50:17 +0100

    week1-final

---
+++ src/pages/login/login.ts
@@ -0,0 +1,31 @@
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+	
+	@ViewChild('username') user;
+	@ViewChild('password') password;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  signIn() {
+  	console.log('Would sign in with ', this.user.value, this.password.value);
+  }
+
+}
---
+++ src/pages/register/register.ts
@@ -0,0 +1,30 @@
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the RegisterPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-register',
+  templateUrl: 'register.html',
+})
+export class RegisterPage {
+
+	@ViewChild('username') user;
+	@ViewChild('password') password;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RegisterPage');
+  }
+
+  registerUser() {
+  	console.log('Would register user with ', this.user.value, this.password.value);
+  }
+}
