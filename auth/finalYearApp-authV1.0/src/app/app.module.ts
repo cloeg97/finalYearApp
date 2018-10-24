1af4e44f1a5c2489d84c18d59dd57ac6b950c03b
CLOE JAMIE MARIE GIBBONS <cloe.gibbons.2016@mumail.ie>
2018-10-24 12:19:04 +0100

    working files for user auth

---
+++ auth/finalYearApp-authV1.0/src/app/app.module.ts
@@ -0,0 +1,68 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+import { CreatePage } from '../pages/create/create';
+import { ViewPage } from '../pages/view/view';
+import { RequestPage } from '../pages/request/request';
+import { WelcomePage } from '../pages/welcome/welcome';
+//added imports 19/10/18
+import * as firebase from 'firebase';
+import { UsersserviceProvider } from '../providers/usersservice/usersservice';
+import { AngularFireModule } from 'angularfire2';
+import {FIREBASE_CONGFIG} from "./app.firebase.config"
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+
+// Initialize Firebase
+export const config = {
+    apiKey: "AIzaSyDpLFX2ybiGPWLgLZiR9RTW8gH3BHFRNCo",
+    authDomain: "ionic-test-cc733.firebaseapp.com",
+    databaseURL: "https://ionic-test-cc733.firebaseio.com",
+    projectId: "ionic-test-cc733",
+    storageBucket: "ionic-test-cc733.appspot.com",
+    messagingSenderId: "709127477872"
+};
+firebase.initializeApp(config);
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    CreatePage,
+    ViewPage,
+    RequestPage,
+    WelcomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONGFIG)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    RegisterPage,
+    CreatePage,
+    ViewPage,
+    RequestPage,
+    WelcomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UsersserviceProvider
+  ]
+})
+export class AppModule {}
---
+++ auth/finalYearApp-authV1.0/src/pages/view/view.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+ 
+/**
+ * Generated class for the ViewPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-view',
+  templateUrl: 'view.html',
+})
+export class ViewPage {
+
+  constructor(public navCtrl: NavController,public navParams: NavParams) {
+ 
+  }
+
+ 
+  
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ViewPage');
+  }
+
+}
